5132b5dd6739568bef345326524bef59c88e4f9b
hyang7 <hyang7@trip.com>
2022-12-02 18:29:00 +0800

    点菜功能

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,21 +1,45 @@
 import { ClassList, DishesList } from "../data";
 import { IClass, IDishes } from "../page/type";
-import axios from './axios';
+import axios from "./axios";
+
+interface AxiosRes {
+    data: ISendEmail
+}
+
+interface ISendEmail {
+    code: number;
+    message: string;
+}
 
 export const getClassList = (): Promise<{
-    classList: IClass[];
-    dishesList: IDishes[];
+	classList: IClass[];
+	dishesList: IDishes[];
 }> => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve({
-                classList: ClassList,
-                dishesList: DishesList,
-            });
-        }, 1000);
-    });
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve({
+				classList: ClassList,
+				dishesList: DishesList,
+			});
+		}, 1000);
+	});
 };
 
-export const sendEmail = (): Promise<{ code: number; message: string }> => {
-    return axios.post("/sendEmail", { to: "haoyang_8_1@163.com", html: "hhh" });
+const getHtml = (dishes: IDishes[]) => {
+	return `<h2>有人点了以下菜品，挥舞你的勺子吧:</h2>
+    <ul>
+        ${dishes.map((d) => {
+			return `<li>${d.name}</li>`;
+		})}
+    </ul>`.replace(/\,/g,'');
+};
+
+export const sendEmail = (
+	dishes: IDishes[]
+): Promise<AxiosRes> => {
+	const html = getHtml(dishes);
+	return axios.post("/sendEmail", {
+		to: ["18390209675@163.com, 872636204@qq.com"],
+		html,
+	});
 };
--- src/page/Footer/index.tsx
+++ src/page/Footer/index.tsx
@@ -2,10 +2,10 @@ import React, { useState, useCallback } from "react";
 import { connect } from "react-redux";
 import { Toast, Loading, ActivityIndicator } from "zarm";
 import {
-    Shopping_Cart_Update,
-    Del_All_Shopping_Cart,
+	Shopping_Cart_Update,
+	Del_All_Shopping_Cart,
 } from "../../redux/action";
-import { sendEmail } from '../../api';
+import { sendEmail } from "../../api";
 import { isValidArray } from "../../utils";
 import MyIcon from "../Component/Icon";
 import { IState } from "../type";
@@ -13,98 +13,119 @@ import DetailModule from "./DetailModule";
 import "./style.scss";
 
 interface IProps extends IState {
-    isShowClass?: boolean;
-    shoppingCartUpdate: (data: any) => void;
-    delAllShoppingCart: () => void;
+	isShowClass?: boolean;
+	shoppingCartUpdate: (data: any) => void;
+	delAllShoppingCart: () => void;
 }
 
 const Footer = ({
-    shoppingCartList,
-    shoppingCartUpdate,
-    delAllShoppingCart,
+	shoppingCartList,
+	shoppingCartUpdate,
+	delAllShoppingCart,
 }: IProps) => {
-    const [isShowDetail, setIsShowDetail] = useState(false);
+	const [isShowDetail, setIsShowDetail] = useState(false);
 
-    const submit = useCallback(() => {
-        if (!isValidArray(shoppingCartList)) {
-            Toast.show({
-                content: "还没点菜呢～",
-                stayTime: 3000,
-            });
-            return;
-        }
+	const submit = useCallback(() => {
+		if (!isValidArray(shoppingCartList)) {
+			Toast.show({
+				content: "还没点菜呢～",
+				stayTime: 3000,
+			});
+			return;
+		}
 
-		sendEmail().then(res=>{
-			console.log('===res',res)
-		}).catch((err)=>{
-			console.log('===err',err)
-		})
-    }, [shoppingCartList]);
+		Loading.show({ content: <ActivityIndicator size="lg" /> });
+		sendEmail(shoppingCartList)
+			.then((res) => {
+				if (res.data.code === 0) {
+					Toast.show({
+						content: "已通知大厨，等吃吧～",
+						stayTime: 3000,
+					});
+					delAllShoppingCart();
+					return;
+				}
 
-    return (
-        <div className="footer-container">
-            <div className="footer-container-content flex">
-                <div
-                    onClick={() => {
-                        if (!isValidArray(shoppingCartList)) {
-                            Toast.show({
-                                content: "还没点菜呢～",
-                                stayTime: 3000,
-                            });
-                            return;
-                        }
-                        setIsShowDetail(true);
-                    }}
-                    className="footer-container-content-left flex"
-                >
-                    <MyIcon className="icon-wan" type="icon-wan" />
-                    {isValidArray(shoppingCartList) ? (
-                        <div className="footer-container-content-left-badge badge">
-                            {shoppingCartList.length}
-                        </div>
-                    ) : null}
-                </div>
-                <div className="footer-container-content-c flex">
-                    <div className="footer-container-content-c-no">
-                        客官点菜吧～
-                    </div>
-                </div>
-                <div
-                    onClick={submit}
-                    className="footer-container-content-right flex"
-                >
-                    通知大厨
-                </div>
-            </div>
-            <DetailModule
-                shoppingCartList={shoppingCartList}
-                setIsShowDetail={setIsShowDetail}
-                visible={isShowDetail}
-                shoppingCartUpdate={shoppingCartUpdate}
-                delAllShoppingCart={delAllShoppingCart}
-            />
-        </div>
-    );
+				Toast.show({
+					content: "哦吼，出错了，快联系开发～",
+					stayTime: 3000,
+				});
+			})
+			.catch((err) => {
+				Toast.show({
+					content: "哦吼，出错了，快联系开发～",
+					stayTime: 3000,
+				});
+			})
+			.finally(() => {
+				Loading.hide();
+			});
+	}, [shoppingCartList]);
+
+	return (
+		<div className="footer-container">
+			<div className="footer-container-content flex">
+				<div
+					onClick={() => {
+						if (!isValidArray(shoppingCartList)) {
+							Toast.show({
+								content: "还没点菜呢～",
+								stayTime: 3000,
+							});
+							return;
+						}
+						setIsShowDetail(true);
+					}}
+					className="footer-container-content-left flex"
+				>
+					<MyIcon className="icon-wan" type="icon-wan" />
+					{isValidArray(shoppingCartList) ? (
+						<div className="footer-container-content-left-badge badge">
+							{shoppingCartList.length}
+						</div>
+					) : null}
+				</div>
+				<div className="footer-container-content-c flex">
+					<div className="footer-container-content-c-no">
+						客官点菜吧～
+					</div>
+				</div>
+				<div
+					onClick={submit}
+					className="footer-container-content-right flex"
+				>
+					通知大厨
+				</div>
+			</div>
+			<DetailModule
+				shoppingCartList={shoppingCartList}
+				setIsShowDetail={setIsShowDetail}
+				visible={isShowDetail}
+				shoppingCartUpdate={shoppingCartUpdate}
+				delAllShoppingCart={delAllShoppingCart}
+			/>
+		</div>
+	);
 };
 
 const mapStateToProps = (state: IState) => {
-    return state;
+	return state;
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        shoppingCartUpdate: (payload) => {
-            dispatch({
-                type: Shopping_Cart_Update,
-                payload,
-            });
-        },
-        delAllShoppingCart: (payload) => {
-            dispatch({
-                type: Del_All_Shopping_Cart,
-            });
-        },
-    };
+	return {
+		shoppingCartUpdate: (payload) => {
+			dispatch({
+				type: Shopping_Cart_Update,
+				payload,
+			});
+		},
+		delAllShoppingCart: (payload) => {
+			dispatch({
+				type: Del_All_Shopping_Cart,
+			});
+		},
+	};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Footer));
