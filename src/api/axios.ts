e4f1e533bef6dd2065f1ba8e0b8e884dc0c75a2e
hyang7 <hyang7@trip.com>
2022-12-07 20:51:16 +0800

    proxy

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,11 +2,9 @@ import axios from "axios";
 
 // 创建实例时配置默认值
 const instance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://116.204.113.145:3000",
     withCredentials: true,
     timeout: 10000,
 });
-
-console.log('===process.env', process.env.NODE_ENV)
 
 export default instance;
